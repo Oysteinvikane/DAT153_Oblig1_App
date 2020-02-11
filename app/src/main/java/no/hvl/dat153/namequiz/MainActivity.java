c7d4a1b5a01b4e9d1a555c34db2ed9f775a71f62
Kjetil Dyrland <kjetil.dyrland@gmail.com>
2020-02-11 16:28:30 +0100

    added shared preferences

--- app/src/main/java/no/hvl/dat153/namequiz/MainActivity.java
+++ app/src/main/java/no/hvl/dat153/namequiz/MainActivity.java
@@ -7,14 +7,24 @@
 package no.hvl.dat153.namequiz;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class MainActivity extends AppCompatActivity {
-
-
+    private SharedPreferences sharedPreferences;
+    private EditText usernameEditText;
+    private TextView usernameTextView;
+    private TextInputLayout inputLayout;
+    private SharedPreferences.Editor editor;
+    private String user;
     /**
      *
      * @param savedInstanceState
@@ -24,6 +34,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        usernameEditText = findViewById(R.id.usernameEditText);
+        usernameTextView = findViewById(R.id.appName);
+        inputLayout = findViewById(R.id.nameInputLayout);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        
+
+        updateView();
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateView();
     }
         //Metode som sender deg til DatabaseActivity
         public void moveToDatabaseActivity(View view) {
@@ -41,7 +67,32 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
 
     }
+    public void updateView() {
+        String username = sharedPreferences.getString("username", null);
+        if (username != null) {
+            usernameEditText.setVisibility(View.INVISIBLE);
+            usernameTextView.setText("Welcome " + username.trim());
+            inputLayout.setVisibility(View.INVISIBLE);
+        } else {
+            inputLayout.setVisibility(View.VISIBLE);
+            usernameEditText.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void updateName(View view) {
 
 
+        String username = usernameEditText.getText().toString().trim();
+        usernameEditText.setText("");
+        if (!username.equals("")) {
+            editor.putString("username", username);
+        } else {
+            editor.remove("username");
+        }
+        editor.commit();
+        updateView();
+
+
+    }
 
 }
